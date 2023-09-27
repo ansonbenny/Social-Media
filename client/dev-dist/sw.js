7c22dc7f514bef0fb9f9bdb341d2489ce82acf91
anson <ansonbenny166@gmail.com>
2023-09-27 15:34:09 +0530

    vite for production

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.egovi9f9ev"
+    "revision": "0.9hjslak0ul8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,64 +1,68 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        "theme_color": "#fff",
-        "background_color": "#fff",
-        "display": "fullscreen",
-        "scope": "/",
-        "start_url": "/",
-        "name": "Soft Chat",
-        "short_name": "Soft Chat",
-        "icons": [
-          {
-            "src": "/icons/icon-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-          },
-          {
-            "src": "/icons/icon-256x256.png",
-            "sizes": "256x256",
-            "type": "image/png"
-          },
-          {
-            "src": "/icons/icon-384x384.png",
-            "sizes": "384x384",
-            "type": "image/png"
-          },
-          {
-            "src": "/icons/icon-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          }
-        ]
-      }
-    })],
-  server: {
-    host: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-      "/files": {
-        target: "http://localhost:5000/files",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/files/, ""),
-      },
-      "/socket.io": {
-        target: "ws://localhost:5000",
-        ws: true,
+export default ({ mode }) => {
+  const env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  return defineConfig({
+    plugins: [
+      react(),
+      VitePWA({
+        devOptions: {
+          enabled: true
+        },
+        manifest: {
+          "theme_color": "#fff",
+          "background_color": "#fff",
+          "display": "fullscreen",
+          "scope": "/",
+          "start_url": "/",
+          "name": "Soft Chat",
+          "short_name": "Soft Chat",
+          "icons": [
+            {
+              "src": "/icons/icon-192x192.png",
+              "sizes": "192x192",
+              "type": "image/png"
+            },
+            {
+              "src": "/icons/icon-256x256.png",
+              "sizes": "256x256",
+              "type": "image/png"
+            },
+            {
+              "src": "/icons/icon-384x384.png",
+              "sizes": "384x384",
+              "type": "image/png"
+            },
+            {
+              "src": "/icons/icon-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png"
+            }
+          ]
+        }
+      })],
+    server: {
+      host: true,
+      proxy: {
+        "/api": {
+          target: `${env.BACK_END}/api`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+        "/files": {
+          target: `${env.BACK_END}/files`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/files/, ""),
+        },
+        "/socket.io": {
+          target: `ws://${env?.BACK_END.substring(env?.BACK_END.indexOf("://") + 3, env?.BACK_END?.length)}`,
+          ws: true,
+        },
       },
     },
-  },
-});
+  });
+}
